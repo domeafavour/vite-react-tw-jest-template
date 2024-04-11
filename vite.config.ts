4824972ddd40c303388ed11c1ab67c696b4351c5
zhanghaojie <1455123087@qq.com>
2024-04-11 19:03:20 +0800

    Add base path for GitHub Pages deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // for github pages
+  base: 'vite-react-tw-jest-template',
   plugins: [react()],
 });
