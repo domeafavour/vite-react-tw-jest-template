8597f11199ef620a2e7e846e5a1eda7afa8efeea
zhanghaojie <1455123087@qq.com>
2024-04-12 10:11:15 +0800

    Remove some build warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./**/*.{html}', './src/**/*.{html,js,ts,jsx,tsx}'],
+  content: ['./**/*.html', './src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // for github pages
-  base: 'vite-react-tw-jest-template',
+  base: '/vite-react-tw-jest-template/',
   plugins: [react()],
 });
