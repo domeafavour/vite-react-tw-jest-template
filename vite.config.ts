2ecc87a1b05ad9e95ad88cb46ff61d7d8c890c41
zhanghaojie <1455123087@qq.com>
2024-04-15 13:47:03 +0800

    Add vite-tsconfig-paths package to package.json and pnpm-lock.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // for github pages
   base: '/vite-react-tw-jest-template/',
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 });
